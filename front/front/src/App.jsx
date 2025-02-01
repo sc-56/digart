f5f11afa8e8927b94a617331c00d9968d3e12dbe
sc <apple@laptop.local>
2025-02-01 15:19:52 +0800

    Navigator

--- front/front/src/App.jsx
+++ front/front/src/App.jsx
@@ -8,6 +8,7 @@ function App() {
 
   return (
     <>
+      
       <div>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -16,6 +17,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
+
       <h1>Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
@@ -35,6 +37,30 @@ function App() {
 export default App
 
 /*
-CSS and HTML are all included in the  
+
+This app runs by "npm run dev" installed by Vite.  
+
+The page has several components. From the top to the bottom, there are two components which are two svg images. svg is for Scalable Vector Graphcs defined by XML language. It wouldn't lose the quality. SVG itself can be interactive using CSS or Javascript.  
+
+In the App, the interaction and animiation is achieved by CSS. In the CSS, using the followed code to define the animation. 
+
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.react:hover {
+  filter: drop-shadow(0 0 2em #61dafbaa);
+}
+
+For the counter, the app uses the code below:
+
+const [count, setCount] = useState(0);
+useState() is one state hook, which is one function used in React.js to manage about the value of state. The value can be saved in the variable returned from this function. 
+
+=> is the representation of function. Therefore, the parameter of setCount() is one function, in which the rule of updating the count should be defined appropriately.  
+
+done: I have understood the main logic of this.
+
+to-do: build my own component of navigator.
+According to the design portfolio, there is one navigator one the head of the page. 
 
 */
---
+++ front/front/src/Nav.jsx
@@ -0,0 +1,20 @@
+import "./Nav.css"
+
+function Navigator() {
+
+    return (
+        <>
+            <nav class="navbar">
+                <span id="title">DigArt</span><span id="sub-title"> : Nonexistent road exists in the world of digital art. </span>
+            </nav>
+        </>
+    )
+}
+
+export default Navigator
+
+/*
+
+The returned value is one object or JSX when using return (). It could be defined that XML is equal to object to some degree. 
+
+*/
