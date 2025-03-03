a853bd640f740a2c29a94ffbbf0a28a6d3b6cb68
sc <apple@laptop.local>
2025-03-03 21:07:03 +0900

    Change something

--- 1-front/src/components/navigator/Nav.jsx
+++ 1-front/src/components/navigator/Nav.jsx
@@ -2,7 +2,11 @@ import "./Nav.css"
 import Image from "/Users/apple/Documents/0-Craft/digart/1-front/public/logs.svg";
 
 const Navigator = () => {
-    return <img src={Image} alt="Description" width="100" />
+    return (
+        <nav id="container-nav">
+            <img  src={Image} alt="Description" width="100" />
+        </nav>
+    );
 };
 
 export default Navigator;
--- 1-front/src/main.jsx
+++ 1-front/src/main.jsx
@@ -3,13 +3,17 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import Navigator from './components/navigator/Nav.jsx'
 import Contentor from './components/contentor/Con.jsx'
+import Sky from './components/nightsky/Sky.jsx'
 
 const root = createRoot(document.getElementById('root'));
 root.render(
   <StrictMode>
     <Navigator />
-    <div id="container">
-      <Contentor />
+
+    <div id="container-1">
+      <div class="column" id="sky">Sky</div>
+      <div class="column" id="contentor">Content</div>
     </div>
+
   </StrictMode>,
 )
